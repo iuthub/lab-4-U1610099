a483e3f092a876647e3425a4d65d9fe2fc016ac4
U1610099 <javlonchik06@gmail.com>
2019-03-10 23:24:26 +0500

    lab 4

---
+++ musicViewerReturn.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+ "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+		<title>Music Viewer</title>
+		<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+		<link rel="stylesheet" type="text/css" href="viewer.css" />
+	</head>
+
+	<body>
+		<div id="header">
+			<h1>190M Music Playlist Viewer</h1>
+			<h2>Search Through Your Playlists and Music</h2>
+		</div>
+		
+		
+		<div id="listArea">
+			<ul id="musicList">
+			</ul>
+		</div>
+
+	</body>
+
+</html>
